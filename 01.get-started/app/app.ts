316b1683bc7268613e44f18a8c5357c824ad3688
DalatCoder <hieuntctk42@gmail.com>
2020-08-12 19:34:39 +0700

    01.Function annotation

--- 01.get-started/app/app.ts
+++ 01.get-started/app/app.ts
@@ -1,15 +1,37 @@
 function startGame() {
   // starting a new game
 
-  let playerName: string = 'Hieu';
+  let playerName: string | undefined = getInputValue('playername');
   logPlayer(playerName);
 
-  var messagesElement = document.getElementById('messages');
-  messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+  postScore(100, playerName);
 }
 
-function logPlayer(name: string): void {
+function logPlayer(name: string = 'MultiMath Player'): void {
   console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementID: string): string | undefined {
+  const inputElement: HTMLInputElement = document.getElementById(
+    elementID
+  ) as HTMLInputElement;
+
+  if (inputElement.value === '') {
+    return undefined;
+  }
+
+  return inputElement.value;
+}
+
+function postScore(
+  score: number,
+  playerName: string = 'MultiMath Player'
+): void {
+  const scoreElement: HTMLElement | null = document.getElementById(
+    'postedScores'
+  );
+
+  scoreElement!.innerText = `${score} - ${playerName}`;
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
